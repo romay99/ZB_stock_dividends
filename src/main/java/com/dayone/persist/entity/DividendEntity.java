4c8d1c2010e63f8a9ce27d07bd4426c09070ceb9
SooBin_Lee <i_romay@naver.com>
2024-12-02 18:49:45 +0900

    Feat : 회사 이름받아서 회사의 메타정보와 배당금 정보 반환하는 메서드 구현 완료

--- src/main/java/com/dayone/persist/entity/DividendEntity.java
+++ src/main/java/com/dayone/persist/entity/DividendEntity.java
@@ -20,7 +20,6 @@ import java.time.LocalDateTime;
         }
 )
 public class DividendEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/dayone/service/FinanceService.java
+++ src/main/java/com/dayone/service/FinanceService.java
@@ -1,26 +1,58 @@
 package com.dayone.service;
 
+import com.dayone.exception.impl.NoCompanyException;
+import com.dayone.model.Company;
+import com.dayone.model.Dividend;
 import com.dayone.model.ScrapedResult;
 import com.dayone.model.constants.CacheKey;
+import com.dayone.persist.CompanyRepository;
+import com.dayone.persist.DividendRepository;
+import com.dayone.persist.entity.CompanyEntity;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Service
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class FinanceService {
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
 
     @Cacheable(key = "#companyName", value = CacheKey.KEY_FINANCE)
     public ScrapedResult getDividendByCompanyName(String companyName) {
         // 1. 회사명을 기준으로 회사 정보를 조회
-
+        CompanyEntity company = companyRepository.findByName(companyName).orElseThrow(
+                () -> new NoCompanyException()
+        );
         // 2. 조회된 회사 ID 로 배당금 정보 조회
+        ScrapedResult scrapedResult = new ScrapedResult();
+        scrapedResult.setCompany(
+                Company.builder()
+                        .ticker(company.getTicker())
+                        .name(company.getName())
+                        .build()
+        );
+
+        List<Dividend> list = dividendRepository.findAllByCompanyId(company.getId()).stream().map(
+                a -> {
+                    Dividend div = new Dividend();
+                    div.setDividend(a.getDividend());
+                    div.setDate(a.getDate());
+                    return div;
+                }).collect(Collectors.toList());
+
+        scrapedResult.setDividends(list);
 
         // 3. 결과 조합 후 반환
-
-        throw new NotYetImplementedException();
+        return scrapedResult;
     }
 }
