c115110b41b1330c05fae097db5accc1de537fa4
SooBin_Lee <i_romay@naver.com>
2024-12-07 13:34:59 +0900

    Fix : 스크랩 안되는 현상 해결

--- src/main/java/com/dayone/scraper/YahooFinanceScraper.java
+++ src/main/java/com/dayone/scraper/YahooFinanceScraper.java
@@ -12,7 +12,6 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +19,7 @@ import java.util.List;
 @Component
 public class YahooFinanceScraper implements Scraper {
 
-    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1mo";
+    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/%s/history/?frequency=1mo&period1=%d&period2=%d";
     private static final String SUMMARY_URL = "https://finance.yahoo.com/quote/%s?p=%s";
 
     private static final long START_TIME = 86400;   // 60 * 60 * 24
@@ -37,7 +36,7 @@ public class YahooFinanceScraper implements Scraper {
             Connection connection = Jsoup.connect(url);
             Document document = connection.get();
 
-            Elements parsingDivs = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Elements parsingDivs = document.select("table.yf-j5d1ld.noDl");
             Element tableEle = parsingDivs.get(0);  // table 전체
 
             Element tbody = tableEle.children().get(1);
@@ -83,8 +82,8 @@ public class YahooFinanceScraper implements Scraper {
         try {
             Document document = Jsoup.connect(url).get();
 
-            Element titleEle = document.getElementsByTag("h1").get(0);
-            String title = titleEle.text().split(" - ")[1].trim();
+            Element titleElement = document.selectFirst("h1.yf-xxbei9");
+            String title = titleElement.text();
 
             return new Company(ticker, title);
         } catch (IOException e) {
